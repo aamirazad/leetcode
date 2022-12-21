62bab83b375158a87cd4defca102a9b825acd6bc
Aamir Azad <aamirmazad@gmail.com>
2022-12-20 21:33:02 -0500

    How many? What input?

--- problems/two-sum.c
+++ problems/two-sum.c
@@ -3,9 +3,10 @@
 
 int main(void)
 {
-    int input[4];
-    input = get_int("Input: ");
-    int target = get_int("Target: ");
-    printf("%i\n", input);
-    printf("%i", target);
+    int n = get_int("How many numbers? ");
+    int input[n];
+    for (int i = 0; i < n ; i++)
+    {
+        input[i] = get_int("Input %i: ", i);
+    }
 }
